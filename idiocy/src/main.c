5ba5e13ba146dc5d4124f4d823679eb46cd2196e
Gaël PORTAY <gael.portay@gmail.com>
2022-10-04 13:13:34 +0200

    Make idiocy a graphic application

--- idiocy/src/main.c
+++ idiocy/src/main.c
@@ -8,6 +8,8 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/display.h>
+#include <lvgl.h>
 
 static const struct device *get_bme280_device(void)
 {
@@ -51,6 +53,8 @@ static const struct device *get_htu21d_device(void)
 void main(void)
 {
 	const struct device *bme280_dev, *bh1750_dev, *htu21d_dev;
+	const struct device *display_dev;
+	lv_obj_t *label;
 
 	bme280_dev = get_bme280_device();
 	if (bme280_dev == NULL)
@@ -63,6 +67,16 @@ void main(void)
 	htu21d_dev = get_htu21d_device();
 	if (htu21d_dev == NULL)
 		return;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev))
+		return;
+
+	label = lv_label_create(lv_scr_act());
+	lv_obj_align(label, LV_ALIGN_TOP_LEFT, 0, 0);
+
+	lv_task_handler();
+	display_blanking_off(display_dev);
 
 	while (1) {
 		struct sensor_value temp, press, light, humidity, temp2;
@@ -84,6 +98,12 @@ void main(void)
 		      light.val2, humidity.val1, humidity.val2, temp2.val1,
 		      temp2.val2);
 
+		lv_label_set_text_fmt(label, "temp: %d.%06d\npress: %d.%06d\nlight: %d.%06d\nhumidity: %d.%06d\ntemp2: %d.%06d\n",
+		      temp.val1, temp.val2, press.val1, press.val2, light.val1,
+		      light.val2, humidity.val1, humidity.val2, temp2.val1,
+		      temp2.val2);
+
+		lv_task_handler();
 		k_sleep(K_MSEC(1000));
 	}
 }
